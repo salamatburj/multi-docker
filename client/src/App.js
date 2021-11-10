08d2a4e78f894bae8141998cafd0e3ee91bec6ec
Salamat Burzhuev <salamat@nus.edu.sg>
2021-11-10 16:18:09 +0800

    app.js update title remove link

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title"> Fib calculator </h1>
+
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
